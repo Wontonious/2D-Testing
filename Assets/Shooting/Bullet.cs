b41f064a5ca8722e7694db3ff85393ed412714df
Wontonious <48232238+Wontonious@users.noreply.github.com>
2020-11-19 08:16:41 -0800

    yes

---
+++ Assets/Shooting/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+    public int damage = 10;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 1.10f);
+        Destroy(gameObject);
+
+        Enemy badGuy = collision.gameObject.GetComponent<Enemy>();
+        if(collision.gameObject.tag == "Enemy")
+        {
+            badGuy.TakeDamage(damage);
+        }
+    }
+}
